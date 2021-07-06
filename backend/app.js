9ca6ffcd886647ca894ebe98af7bf36d74019bbe
DevHÿllen <64634619+Jequi59@users.noreply.github.com>
2021-07-06 11:30:57 +0200

    ...

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+const Thing = require('./models/thing');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://jquievreuxdev:Erin25012017@cluster0.aejzm.mongodb.net/testfs',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use(cors())
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
- app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-      message: 'Objet créé !'
-    });
+app.post('/api/stuff', (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing ({
+    ...req.body
   });
+  thing.save()
+    .then(() => res.status(201).json({message : "Objet enregistré !"}))
+      .catch(() => res.status(400).json({error}))
+});
 
 app.use('/api/stuff', (req, res, next) => {
     const stuff = [
